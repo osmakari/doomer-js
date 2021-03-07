3f8a3f82dcc2804b3effc9eea750852221bc43ca
Oskari Seppa <M0375@student.jamk.fi>
2021-03-07 15:20:14 +0200

    Changed build script

---
+++ build.js
@@ -0,0 +1,13 @@
+const minify = require('minify');
+const fs = require('fs');
+
+fs.writeFileSync("min/doomer.min.js", "");
+
+fs.readdir('./src', (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+        minify('./src/' + file).then((str) => {
+            fs.appendFileSync("min/doomer.min.js", str);
+        })
+    });
+});
