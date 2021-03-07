d2e7967ad4add32d69561d85b5f9514834487f9d
Oskari Seppa <M0375@student.jamk.fi>
2021-03-07 14:48:47 +0200

    Changed HTML

--- src/i_main.js
+++ src/i_main.js
@@ -1,1 +1,20 @@
+// MAIN - initializes the game
+function I_Main () {
+    canvas = document.getElementById("doom-canvas");
+    ctx = canvas.getContext("2d");
 
+    // TODO: ADD MULTIPLIER
+    canvas.width = SCREENWIDTH;
+    canvas.height = SCREENHEIGHT;
+
+    canvas.style.width = SCREENWIDTH;
+    canvas.style.height = SCREENHEIGHT;
+
+    D_DoomMain();
+}
+
+
+// JS onload
+window.onload = () => {
+    I_Main();
+};
---
+++ src/js_def.js
@@ -0,0 +1,11 @@
+/**
+ * @type {HTMLCanvasElement}
+ * @description Game canvas
+ */
+let canvas = null;
+
+/**
+ * @type {CanvasRenderingContext2D}
+ * @description Rendering context
+ */
+let ctx = null;
