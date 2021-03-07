7320e5a27e75ff516f9f5aa9a0ee4042ac42c9ff
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-03-07 15:29:40 +0200

    Enums

--- src/doomdef.js
+++ src/doomdef.js
@@ -7,3 +7,17 @@ const SCREENHEIGHT = 200
  * tied to the scale implied by the graphics 
  * */ 
 const SCREEN_MUL = 1
+
+
+//TODO: ENUMS 
+let gamestate_t = {
+    GS_LEVEL: 0,
+    GS_INTERMISSION: 1,
+    GS_FINALE: 2,
+    GS_DEMOSCREEN: 3
+}
+
+
+let powertype_t = {
+
+}
